fa1135be368d79270968d3124aec7961dc9d462e
Duyen Do Ky <84983247+KyDuyenDo@users.noreply.github.com>
2026-01-27 14:58:13 +0700

    feat: apply exception filter (#3)
    * feat: apply exception filter
    
    * chore: clean

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,14 +1,14 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { User } from "./entities/user.entity";
 import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-    constructor(@InjectRepository(User) private readonly userRepository: Repository<User>) {}
+    constructor(@InjectRepository(User) private readonly userRepository: Repository<User>) { }
     async getUserById(userId: string): Promise<User> {
-        const user = await this.userRepository.findOne({where: { id: userId}})
-        if(!user){ throw new Error('User not found') } // Exception there
+        const user = await this.userRepository.findOne({ where: { id: userId } })
+        if (!user) { throw new NotFoundException() }
         return user
     }
 }
