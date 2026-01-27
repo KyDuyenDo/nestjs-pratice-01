c103627148829e6d8ffd82d3099b033d29cd2ba9
Duyen <duyenk200@gmail.com>
2026-01-27 13:26:56 +0700

    feat: set up logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,33 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configValidationSchema from './common/config/validation.schema.config';
 import { ConfigModule } from '@nestjs/config';
-import configValidationSchema from './config/configValidationSchema ';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/users/user.module';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import databaseConfig from './modules/database/database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(
-    {
-      envFilePath: '.env',
-      isGlobal: true,
-      validationSchema: configValidationSchema
-    }
-  )],
+  imports: [
+    databaseConfig,
+    ConfigModule.forRoot(
+      {
+        envFilePath: '.env',
+        isGlobal: true,
+        validationSchema: configValidationSchema
+      }),
+    AuthModule,
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes('*');
+  }
+}
---
+++ src/common/middleware/logger.middleware.ts
@@ -0,0 +1,16 @@
+import { Injectable, NestMiddleware } from "@nestjs/common";
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+    use(req: Request, res: Response, next: NextFunction) {
+        const logger = {
+            param: req.params,
+            query: req.query,
+            body: req.body,
+            header: req.header,
+        };
+        console.log(logger);
+        next();
+    }
+}
