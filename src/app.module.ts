0da646430754df7a0a5f1f4e2018feec6cb7161a
Duyen <duyenk200@gmail.com>
2026-01-27 09:26:41 +0700

    feat: set up config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(
+    {
+      envFilePath: '.env',
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+        NODE_ENV: Joi.string().valid('development', 'production', 'test', 'provision').default('development'),
+        APP_PORT: Joi.number().required()
+      })
+    }
+  )],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
