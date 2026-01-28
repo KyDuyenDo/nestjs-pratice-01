8a5666a720488faea235da415c7e10e976bad679
Duyen <duyenk200@gmail.com>
2026-01-28 08:06:37 +0700

    chore: custom decorator

--- src/modules/auth/dto/login.dto.ts
+++ src/modules/auth/dto/login.dto.ts
@@ -1,7 +1,9 @@
 import { IsEmail, IsString } from "class-validator";
+import { IsValidEmail } from "src/common/decorators/login.decorator";
 
 export class LoginDto {
     @IsEmail()
+    @IsValidEmail()
     email: string
     @IsString()
     password: string
